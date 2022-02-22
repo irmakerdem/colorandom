94cb1594fb72515cddf35c0ed004a8c58630da85
Irmak Erdem <irmakerdem9@gmail.com>
2022-02-22 15:40:48 -0700

    Add initial branch outline

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// chores/0-initial-page-layout
+// feature/0-initial-page-layout
+// feature/1-create-classes
+// feature/2-show-random-palette
+// feature/3-save-palette
+// feature/4-lock-colors
+// feature/5-delete-saved-palette
+// feature/initial-readme
