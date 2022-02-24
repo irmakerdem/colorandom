1c4bc90ba583a82dde84509c4969c0f737479de9
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-02-24 17:01:43 -0600

    Start JS functionality

--- color.js
+++ color.js
@@ -1,10 +1,11 @@
 class Color {
   constructor() {
     this.locked = false;
+    // this.color = #83b2ff || getRandomColor();
   }
 };
 
 
-lockColors(){
-  //if color.unlocked = true, can run get randomColor
-}
+// lockColors(){
+//   //if color.unlocked = true, can run get randomColor
+// };
--- scripts.js
+++ scripts.js
@@ -9,17 +9,31 @@
 // extensions if we have time
 
 
+// var displayColor =
+
+
 
 //QUERY SELECTORS//
-var square1 = document.querySelector("#boxOne")
+var square1 = document.querySelector("#boxOne");
+var randomButton = document.querySelector(".random-button");
+var allBoxes = document.querySelectorAll(".box");
+var hexCodes = document.querySelector(".hex-code");
+var colorBlocks = document.querySelector(".color-block")
+//EVENT LISTENERS//
+window.addEventListener("load", getRandomColor);
+randomButton.addEventListener("click", getRandomColor);
 
 
+//FUNCTIONS//
+function insertHex() {
+  colorBlocks.innerHTML = getRandomColor()
+};
 
 function getRandomColor() {
+  event.preventDefault();
   var allCharacters = 'ABCDEF0123456789';
   var hex = '#';
   for (var i = 0; i < 6; i++) {
     hex = hex + allCharacters[Math.floor(Math.random() * 16)];
   }
-  return hex;
-}
+};
