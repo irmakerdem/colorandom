222a33f39cb47d212b1b5247f5e32c16ca9aeca6
Irmak Erdem <irmakerdem9@gmail.com>
2022-02-25 16:30:49 -0700

    Add random color functionality to all squares

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-// chores/0-initial-page-layout
-// feature/0-initial-page-layout
 // feature/1-create-classes
 // feature/2-show-random-palette
 // feature/3-save-palette
@@ -11,6 +9,9 @@
 //QUERY SELECTORS//
 var square1 = document.querySelector("#boxOne");
 var square2 = document.querySelector("#boxTwo");
+var square3 = document.querySelector("#boxThree");
+var square4 = document.querySelector("#boxFour");
+var square5 = document.querySelector("#boxFive");
 var randomButton = document.querySelector(".random-button");
 var allBoxes = document.querySelectorAll(".box");
 var hexCodes = document.querySelector(".hex-code");
@@ -24,7 +25,11 @@ randomButton.addEventListener("click", getNewPalette);
 function getNewPalette(){
   var newPalette = new Palette()
   //for loop over every color in palette and
-    square1.style.backgroundColor = newPalette.colors[0].hex
+    square1.style.backgroundColor = newPalette.colors[0].hex;
+    square2.style.backgroundColor = newPalette.colors[1].hex;
+    square3.style.backgroundColor = newPalette.colors[2].hex;
+    square4.style.backgroundColor = newPalette.colors[3].hex;
+    square5.style.backgroundColor = newPalette.colors[4].hex;
 };
 // event.preventDefault();
 
