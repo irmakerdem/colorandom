6bd42f1d0d960d633f783bd713f1a603a69bf280
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-02-27 13:48:25 -0600

    Refactor lock functionality

--- palette.js
+++ palette.js
@@ -3,4 +3,11 @@ class Palette {
     this.colors = [color1, color2, color3, color4, color5];
     this.id = Date.now();
   }
+  giveIDs(){
+    //give Ids to colors in new palette that match html id for colorblock, index =
+    var colorBlockIDs = ["colorBlockOne", "colorBlockTwo", "colorBlockThree", "colorBlockFour", "colorBlockFive"]
+    for(var i = 0; i < 5; i++){
+    this.colors[i].id = colorBlockIDs[i];
+  }
+ }
 }
